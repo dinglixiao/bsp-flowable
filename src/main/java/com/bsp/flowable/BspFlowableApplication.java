e210727fd849c140449a82265f1dca013f5bef48
dinglixiao <1713716789@qq.com>
2019-05-17 14:18:10 +0800

    Initial commit

---
+++ src/main/java/com/bsp/flowable/BspFlowableApplication.java
@@ -0,0 +1,13 @@
+package com.bsp.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BspFlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BspFlowableApplication.class, args);
+    }
+
+}
